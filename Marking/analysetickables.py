e46f1861b756bf1b7673601687177efc70742d92
drvinceknight <vincent.knight@gmail.com>
2013-10-24 10:02:34 +0100

    proper documentation

--- Marking/analysetickables.py
+++ Marking/analysetickables.py
@@ -9,14 +9,33 @@ import matplotlib.pyplot as plt
 class Student():
     """
     A class for a student
+
+    Attributes:
+        name: A string
+        ticks: A list of booleans
     """
     def __init__(self,name):
+        """
+        Initialises the student
+
+        Arguments: name (a string)
+        """
         self.name = name
         self.ticks = []
 
 class Group():
     """
     A class for a tutor group
+
+    Arguments:
+        number: integer (corresponding to sheet number on spreadsheet)
+        data: a raw data file in the format of the spreadsheets (gspread ensures this will be a list)
+
+    Attributes:
+        number: integer (corresponding to sheet number on spreadsheet)
+        students:  a list of Student objects
+        rawdata: the raw csv file read in as a list
+        numberofticks: a list of integers corresponding to the number of ticks of each student
     """
     def __init__(self, number, data):
         self.number = number
@@ -35,24 +54,23 @@ class Group():
 
 
 
+print "Authenticating"
 # Download data
-username = "vincent.knight"
+username = "vincent.knight"  # My gmail username
 password = "mxsvlamsbbrepuqy"  # App specific password
-
-docid = "0Ah_zrw5uAafbdEd4M0RiUWFONW1uUHJjU1lmeExKNkE"
+docid = "0Ah_zrw5uAafbdEd4M0RiUWFONW1uUHJjU1lmeExKNkE"  # The id for the spreadheet
 
 client = gspread.login(username, password)
-spreadsheet = client.open_by_key(docid)
+spreadsheet = client.open_by_key(docid)  # Create a client object
 print "Downloading data"
-fulldata = {}
+fulldata = {}  # A dictionary to hold the data by group number
 for i, worksheet in enumerate(spreadsheet.worksheets()):
-    filename = docid + '-worksheet' + str(i) + '.csv'
-    fulldata[i] = worksheet.get_all_values()
+    fulldata[i] = worksheet.get_all_values()  # Read in the data
 
 print "Collating data"
 groups = []  # Create groups (and students)
 for i in fulldata:
-    groups.append(Group(i, fulldata[i]))
+    groups.append(Group(i, fulldata[i]))  # This initialises groups
 
 groupnames = {0:"P",
               1:"R",
@@ -63,7 +81,7 @@ groupnames = {0:"P",
               6:"W",
               7:"X",
               8:"Y",
-              9:"Z"}
+              9:"Z"}  # For labelling of graphs
 
 print "Plotting data"
 allcounts = []
@@ -75,9 +93,9 @@ for g in groups:
     plt.ylabel("Proportion")
     plt.title("Group %s" % groupname)
     plt.savefig("%s.pdf" % groupname)
-    allcounts += g.numberofticks
+    allcounts += g.numberofticks  # Keep track of total ticks
 
-
+# Plot total histogram
 plt.figure()
 plt.hist(allcounts)
 plt.xlabel("Number of ticks")
