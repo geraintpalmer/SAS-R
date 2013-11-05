90b3d0fbfa1205e853a8893b031b4ef309814189
vince <vincent.knight@gmail.com>
2013-11-05 21:26:09 +0000

    Have started analysing stuff

---
+++ Feedback/analysemidmodulefeedback.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Script to analyse feedback
+"""
+
+class Opinion():
+    def __init__(self, label, positive)
+        self.label = label
+        self.positive = positive
