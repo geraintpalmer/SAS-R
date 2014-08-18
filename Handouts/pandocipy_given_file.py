f75e0e7da776c7540aa5055a329d1ea984fab614
drvinceknight <vincent.knight@gmail.com>
2014-08-18 14:52:44 +0100

    Commiting before moving to try something completely different

--- Handouts/pandocipy_given_file.py
+++ Handouts/pandocipy_given_file.py
@@ -5,6 +5,8 @@ from os import system
 e = argv[1][:-3]
 print e
 
-system("pandoc -s --mathjax='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML' --highlight-style tango " + e +  ".md -o " + e + ".html")
-system("pandoc -s " + e +  ".md -o " + e + ".docx")
-system("pandoc -s --listings -V geometry:margin=1.5cm --latex-engine=xelatex " + e +  ".md -o " + e + ".pdf")
+print "cat %s.md" % e
+system("cat %s.md" % e)
+#system("pandoc -s --mathjax='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML' --highlight-style tango " + e +  ".md -o " + e + ".html")
+#system("pandoc -s " + e +  ".md -o " + e + ".docx")
+#system("pandoc -s --listings -V geometry:margin=1.5cm --latex-engine=xelatex " + e +  ".md -o " + e + ".pdf")
